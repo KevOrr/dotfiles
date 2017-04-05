76567720d8b6823b40b63a2ce72aeb96493524a9
Kevin Orr <kevinorr54@gmail.com>
2017-04-05 18:50:36 -0400

    [spacemacs] Set browser to xdg-open in emacs

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -367,6 +367,8 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-generic))
+ '(browse-url-generic-program "xdg-open")
  '(c-basic-offset 4)
  '(c-default-style
    (quote
