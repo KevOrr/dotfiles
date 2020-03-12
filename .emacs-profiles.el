03153911fea25b9388a511c7014fa8f38766efb3
Kevin Orr <kevinorr54@gmail.com>
2020-03-12 12:13:18 -0400

    [emacs] Add chemacs and create DOOM and Spacemacs profiles

---
+++ .emacs-profiles.el
@@ -0,0 +1,6 @@
+(
+ ("vanilla" . ((user-emacs-directory . "")))
+ ("doom" . #1=((user-emacs-directory . "~/src/doom-emacs")))
+ ("spacemacs" . ((user-emacs-directory . "~/src/spacemacs")))
+ ("default" . #1#)
+ )
