a2f27cef0b0ef6e390e17b1d3554cc204a130e8c
Kevin Orr <kevinorr54@gmail.com>
2020-06-08 16:59:26 -0400

    [doom] Remove package! org-roam because doom has its own modeule flag now

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -3,9 +3,6 @@
 
 (package! flycheck-pos-tip)
 (package! open-junk-file)
-;; (package! org-roam
-;;   ;; why does this need a recipe... It's on MELPA
-;;   :recipe '(:host github :repo "jethrokuan/org-roam"))
 
 (package! org-ref)
 (package! ebib)
