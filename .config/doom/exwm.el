a3e37232d9f17b94e770103dfb2c8501814afd3f
Kevin Orr <kevinorr54@gmail.com>
2021-05-05 14:23:26 -0400

    [doom] [exwm] I have absolutely no memory what this does

--- .config/doom/exwm.el
+++ .config/doom/exwm.el
@@ -43,4 +43,28 @@
    (+private/exwm-runner "polybar" "*Polybar*" "polybar -r main")
    (+private/exwm-runner "dunst" "*Dunst*" "dunst"))
 
-  )
+  (defun +ivy-posframe-display-exwm (str)
+    (ivy-posframe--display str
+                           (lambda (info)
+                             (let* ((workarea (elt exwm-workspace--workareas exwm-workspace-current-index))
+                                    (x (aref workarea 0))
+                                    (y (aref workarea 1))
+
+                                    (fw (aref workarea 2))
+                                    (fh (aref workarea 3))
+
+                                    (pw (plist-get info :posframe-width))
+                                    (ph (plist-get info :posframe-height)))
+
+                               (cons (+ x (/ (- fw pw) 2)) (+ y (/ (- fh ph) 2)))))))
+
+  (setq ivy-posframe-display-functions-alist
+        '((t . +ivy-posframe-display-exwm))
+
+        ivy-posframe-parameters '((parent-frame nil)
+                                  (z-group . above)))
+
+  ;; force set frame-position on every posframe display
+  (advice-add 'posframe--set-frame-position :before
+              (lambda (&rest _)
+                (setq-local posframe--last-posframe-pixel-position nil))))
