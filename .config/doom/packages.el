e73b8e3e43cee1fb6176fd78dd22741fa73a901a
Kevin Orr <kevinorr54@gmail.com>
2021-05-05 14:24:14 -0400

    [doom] Name packages.el sections

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -3,16 +3,20 @@
 
 (package! benchmark-init)
 
+;; util
 (package! flycheck-pos-tip)
 (package! open-junk-file)
 
+;; org
 (package! org-ref)
 (package! ebib)
 (package! org-roam-bibtex)
 (package! org-roam-server)
 (package! ivy-bibtex)
 
+;; git
 (package! git-auto-commit-mode)
 
+;; eaf
 (package! eaf
   :recipe (:host github :repo "manateelazycat/emacs-application-framework"))
