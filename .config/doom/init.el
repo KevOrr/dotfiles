e82fffc1d0cdc3ea6c9267fe9054abd000d6abba
Kevin Orr <kevinorr54@gmail.com>
2021-05-19 16:33:16 -0400

    [doom] Enable python +poetry

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -169,7 +169,7 @@
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +cython +pyenv +lsp +pyright)            ; beautiful is better than ugly
+       (python +cython +pyenv +lsp +pyright +poetry)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
