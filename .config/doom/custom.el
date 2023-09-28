0cdac1c8e726934e45406d9f76f1cf65d422f610
Kevin Orr <kevinorr54@gmail.com>
2023-09-27 20:26:22 -0400

    [doom] Update custom-set-variables (idkwid)

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -3,41 +3,26 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(lsp-haskell-formatting-provider "fourmolu")
  '(safe-local-variable-values
-   '((+format-with . yapf)
-     (+format-with-lsp)
-     (+format-with . :none)
-     (flycheck-clang-language-standard . c++20)
-     (flycheck-gcc-language-standard . c++20)
-     (cider-clojure-cli-global-options "-A:capstone")
-     (cider-figwheel-main-default-options . ":dev")
-     (cider-default-cljs-repl . figwheel-main)
-     (cider-clojure-cli-global-options . "-A:dev")
+   '((flycheck-clang-language-standard . c++20)
+     (+format-with . yapf)
      (gac-automatically-add-new-files-p)
      (gac-automatically-add-new-files-p . t)
      (gac-automatically-push-p)
      (gac-automatically-push-p . t)
      (+org-roam-open-buffer-on-find-file)
-     (+org-roam-open-buffer-on-find-file . t)))
- '(warning-suppress-types
-   '((after-save-hook)
-     (before-save-hook)
-     ((flycheck syntax-checker))
-     (idris2-load-file-success-hook)
-     ((undo discard-info))
-     ((undo discard-info))
-     (before-save-hook)
-     (before-save-hook)
-     (:warning))))
+     (+org-roam-open-buffer-on-find-file . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(agda2-highlight-unsolved-constraint-face ((t (:underline (:style wave :color "orange")))))
+ '(agda2-highlight-unsolved-meta-face ((t (:underline (:style wave :color "red")))))
  '(lsp-headerline-breadcrumb-path-hint-face ((t (:underline nil :inherit lsp-headerline-breadcrumb-path-face))) t)
  '(lsp-headerline-breadcrumb-symbols-hint-face ((t (:underline nil :inherit lsp-headerline-breadcrumb-symbols-face))) t)
  '(lsp-lsp-flycheck-info-unnecessary-face ((t (:foreground "dim gray" :underline nil))) t)
  '(proof-locked-face ((t (:background "gray10"))))
  '(slime-highlight-edits-face ((t (:background "gray10")))))
 (put 'narrow-to-region 'disabled nil)
+(put 'list-threads 'disabled nil)
