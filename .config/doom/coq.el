dcab2ce0d00202eaa7cdacf53ac2d9c82f373aac
Kevin Orr <kevinorr54@gmail.com>
2023-09-27 20:24:07 -0400

    [doom] Start making coq init more async on startup

--- .config/doom/coq.el
+++ .config/doom/coq.el
@@ -9,21 +9,44 @@
 ;; run M-x coq-update-opam-switches and coq-change-compiler will show the updated set of switches.
 
 (require 'dash)
+(require 'consult)
+
+;;; Adapted from @djeis
+(defun thready-process-lines (program &rest args)
+  (with-temp-buffer
+    (let ((proc (make-process
+                 :name program
+                 :buffer (current-buffer)
+                 :stderr nil
+                 :command (cons program args)
+                 :connection-type 'pipe
+                 :sentinel #'ignore)))
+      (while (accept-process-output proc))
+      (unless (eq (process-exit-status proc) 0)
+        (error "%s exited with status %s" program (process-exit-status proc)))
+      (goto-char (point-min))
+      (let (lines)
+        (while (not (eobp))
+          (setq lines (cons (buffer-substring-no-properties
+                             (line-beginning-position)
+                             (line-end-position))
+                            lines))
+          (forward-line 1))
+        (nreverse lines)))))
 
 (defun opam-installed-switches ()
   "Get a list of all installed OPAM switches."
-  (process-lines "opam" "switch" "--safe" "list" "--short"))
+  (thready-process-lines "opam" "switch" "--safe" "list" "--short"))
 
 (defun opam-current-switch ()
   "Get the current OPAM switch."
-  (ignore-errors (car (process-lines "opam" "switch" "--safe" "show"))))
+  (ignore-errors (car (thready-process-lines "opam" "switch" "--safe" "show"))))
 
 (defun opam-config-list (switch package &rest packages)
   "Get config variables for packages in switch"
-  (let ((lines (apply #'process-lines (append (list "opam" "config" "list" "--safe")
-                                              (if switch (list "--switch" switch))
-                                              (list package)
-                                              packages))))
+  (let ((lines (apply #'thready-process-lines (append (list "opam" "config" "list" "--safe")
+                                                      (if switch (list "--switch" switch))
+                                                      (cons package packages)))))
     (save-match-data
       (-non-nil
        (--map
@@ -47,36 +70,73 @@
          (cdr (assoc '("coq" . "bin") coq-vars)))))
     (opam-installed-switches))))
 
-(setq coq-bin-dirs-alist (coq-opam-switches-with-coq-to-bindir))
+
+;; (let ((tr (make-thread (lambda () (process-lines "sh" "-c" "sleep 5; echo hi")))))
+;;   (defun foo ()
+;;     (thread-join tr)))
+
+;; (let ((tr (make-thread
+;;            (lambda ()
+;;              (with-temp-buffer
+;;                (let ((p (make-process
+;;                          :name "sh"
+;;                          :buffer (current-buffer)
+;;                          :stderr nil
+;;                          :command '("sh" "-c" "sleep 5; echo hi")
+;;                          :sentinel #'ignore)))
+;;                  (while (accept-process-output p))
+;;                  (buffer-string)))))))
+;;   (defun foo ()
+;;     (thread-join tr)))
+
+;; let-over-defun doesn't seem to play nicely with either edebug or Doom's eval
+;; module, so let's do this instead
+(defun -make-coq-bin-dirs-alist-getter ()
+  (let ((tr (make-thread
+            (lambda ()
+              (let* ((switches (coq-opam-switches-with-coq-to-bindir))
+                     (switch
+                      (car (--filter (string-prefix-p (concat opam-current-switch " (coq-") (car it))
+                                     switches))))
+                (if switch
+                    (coq-change-compiler (cdr switch))
+                  (warn "Current opam switch %s not found" opam-current-switch))
+                switches))))
+       (val nil))
+   (lambda (&optional update)
+     (cond
+      (update (setq val (coq-opam-switches-with-coq-to-bindir)))
+      ((null val) (setq val (thread-join tr))))
+     val)))
+
+(defvar get-coq-bin-dirs-alist (-make-coq-bin-dirs-alist-getter))
 
 (defun coq-update-opam-switches ()
   "Update the alist of OPAM switches with Coq installed"
   (interactive)
-  (setq coq-bin-dirs-alist (coq-opam-switches-with-coq-to-bindir))
+  (funcall get-coq-bin-dirs-alist)
   (message "OPAM switches updated."))
 
 (defun coq-change-compiler (coq-bin-dir)
   "Change Coq executables to use those in COQ-BIN-DIR."
   (interactive
-   (list (cdr (assoc (ivy-read "Compiler: " coq-bin-dirs-alist
-                               :require-match t
-                               :preselect opam-current-switch)
-                     coq-bin-dirs-alist))))
+   (let* ((alist (funcall get-coq-bin-dirs-alist))
+          (name (consult--read
+                 alist
+                 :prompt "Compiler: "
+                 :require-match t
+                 :default opam-current-switch))
+          (switch (cdr (assoc name alist))))
+     (list switch)))
   (when (stringp coq-bin-dir)
-    (when-let ((opam-current-switch (car (rassoc coq-bin-dir coq-bin-dirs-alist))))
-      (set 'opam-current-switch (car (split-string opam-current-switch))))
+    ;; (when-let ((opam-current-switch (car (rassoc coq-bin-dir (coq-bin-dirs-alist)))))
+    ;;   (set 'opam-current-switch (car (split-string opam-current-switch))))
     (setq coq-compiler (concat coq-bin-dir "/coqc"))
     (setq coq-prog-name (concat coq-bin-dir "/coqtop"))
     (setq coq-dependency-analyzer (concat coq-bin-dir "/coqdep"))
     (message "Using Coq binaries from %s." coq-bin-dir)
     (when (proof-shell-available-p)
       (proof-shell-exit))))
-
-(if-let ((switch
-          (car (--filter (string-prefix-p (concat opam-current-switch " (coq-") (car it))
-                         coq-bin-dirs-alist))))
-    (coq-change-compiler (cdr switch))
-  (warn "Current opam switch %s not found" opam-current-switch))
 
 (use-package! company-coq
   :defer t
