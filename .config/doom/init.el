5e06072b5a4d41ba6c02177126d883359c66f436
Kevin Orr <kevinorr54@gmail.com>
2025-04-21 17:54:50 -0400

    [doom] spell -flyspell +everywhere

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -99,7 +99,7 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       (spell +flyspell +aspell)     ; tasing you for misspelling mispelling
+       (spell +aspell +everywhere)     ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
